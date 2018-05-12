d99a3892a72791d9737ac306494615effdb0733a
mcclurgm <mcgearboulder@gmail.com>
2018-05-12 17:41:15 -0500

    Cleaning up CSV parser; unable to test degrees without data

--- inertial/tests/csv_parse.py
+++ inertial/tests/csv_parse.py
@@ -12,35 +12,40 @@ class data_meters():
     def __init__(self):
         self.degree_data = []
         self.meter_data = []
+        self.get_data_from_csv(sys.argv[1])
+        print(self.degree_data)
+        self.get_meters()
+        self.write_data()
 
-        with open(sys.argv[1], "r") as solo_data:
+    def get_data_from_csv(self, filename):
+        with open(filename, "r") as solo_data:
             data_reader = csv.reader(solo_data, delimiter=",")
             next(data_reader)
             refresh = next(data_reader)
             print(refresh)
             
-            lat_base = float(refresh[0])
-            long_base = float(refresh[1])
+            # lat_base = float(refresh[0])
+            # long_base = float(refresh[1])
             alt_base = float(refresh[2])
             pitch_base = float(refresh[3])
             roll_base = float(refresh[4])
             yaw_base = float(refresh[5])
             time_base = float(refresh[6])
             for refresh in data_reader:
-                lat = float(refresh[0]) - lat_base
-                long = float(refresh[1]) - long_base
+                # lat = float(refresh[0]) - lat_base
+                # long = float(refresh[1]) - long_base
+                lat = float(refresh[0])
+                long = float(refresh[1])
                 alt = float(refresh[2]) - alt_base
                 pitch = float(refresh[3]) - pitch_base
                 roll = float(refresh[4]) - roll_base
                 yaw = float(refresh[5]) - yaw_base
                 time = float(refresh[6]) - time_base
 
-                print([lat, long, alt, pitch, roll, yaw, time])
+                # print([lat, long, alt, pitch, roll, yaw, time])
 
                 self.degree_data.append([lat, long, alt, pitch, roll, yaw, time])
         
-        self.write_data()
-
     # Note: distance calculator has error of +/- 0.5%
     def get_meters(self):
         # print self.degree_data
@@ -53,7 +58,7 @@ class data_meters():
             lat_meters = distance.distance((0,0), lat).meters
             long_meters = distance.distance((0,0), long).meters
 
-            self.result_data.append([lat_meters, long_meters, row[2], row[3]])
+            result_data.append([lat_meters, long_meters, row[2], row[3]])
         self.meter_data = result_data
 
     def write_data(self):
